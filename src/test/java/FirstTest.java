aaf07eccfd17d3f472951e102485ee58ec83cf4d
Teodor Trajchev <teo.trajcev@gmail.com>
2026-08-21 14:22:16 +0200

    first test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.apache.xmlbeans.impl.xb.ltgfmt.TestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstTest{
+
+
+    public void testFirst() throws Exception {
+    }
+}
